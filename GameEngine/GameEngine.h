5f026991f9daa1e07ca3719dc164a79b60fb6c6d
Zack Walton <zackwalton15@gmail.com>
2023-10-04 20:22:40 -0400

    Added testGameStates free function

--- GameEngine/GameEngine.h
+++ GameEngine/GameEngine.h
@@ -1,10 +1,10 @@
 #ifndef GAMEENGINE_H
 #define GAMEENGINE_H
 
-
 class GameEngine {
 
 };
 
+void testGameStates();
 
 #endif //GAMEENGINE_H
---
+++ GameEngine/GameEngineDriver.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <GameEngine.h>
+
+using namespace std;
+
+void testGameStates() {
+    cout << ">>> Testing GameEngine <<<" << endl;
+}
