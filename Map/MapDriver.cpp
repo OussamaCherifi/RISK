d5e59877ea4612954198f82171c0048ca9db40df
Oussama.C <oussamacherifi7@gmail.com>
2023-11-09 17:19:28 -0500

    Made it so that it can open the file using an absolute path.

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Map.h"
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
 
@@ -10,16 +11,33 @@ int main() {
 }
 
 void MapDriver::testLoadMaps(){
-
     MapLoader driver = MapLoader();
 
-    string MapName = "../mapFiles/Roman Empire.map";
-//    string MapName2 = "../mapFiles/Roman Empire.map";
-    // Create and test a map
+    // Get the current working directory, which is typically the directory of the C++ project
+    std::filesystem::path projectPath = std::filesystem::current_path();
+
+    // Directory to remove from the path
+    std::string directoryToRemove = "cmake-build-default-mingw";
+
+    // Remove the directory from the path if it exists
+    if (projectPath.filename() == directoryToRemove) {
+        projectPath = projectPath.parent_path();
+    }
+
+    // Append the desired path to it
+    projectPath /= "Map\\mapFiles\\mapFileTest.txt";
+
+    // Convert the path to a string
+    std::string absolutePathStr = projectPath.string();
+
+    // Print the absolute path
+    std::cout << "Absolute Path: " << absolutePathStr << std::endl;
+
+      // Create and test a map
         Map* mapTest = new Map();
 
-        bool testResult = driver.createMapFromFile(MapName, mapTest);
-
+        bool testResult = driver.createMapFromFile(absolutePathStr, mapTest);
+//    C:\Users\oussa\OneDrive\Bureau\fall 2023\comp345\risk_a2\Map\mapFiles\mapFileTest.txt
         if (testResult) {
             cout << "Successful creation of a map from " << mapTest << endl;
             if (mapTest->validate()) {
