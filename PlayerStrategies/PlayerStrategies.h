79e3515051a986c5d95f6c45ab53293ea83bfacd
Zack Walton <zackwalton15@gmail.com>
2023-11-29 13:49:53 -0500

    Added more player strategies

--- PlayerStrategies/PlayerStrategies.h
+++ PlayerStrategies/PlayerStrategies.h
@@ -7,34 +7,86 @@
 using namespace std;
 
 class Player;
+
 class Territory;
 
-class PlayerStrategy{
+class PlayerStrategy {
 protected:
     Player *p;
 public:
     virtual vector<Territory *> toAttack() = 0;
+
     virtual vector<Territory *> toDefend() = 0;
+
     virtual void issueOrder() = 0;
+
     virtual string getType() = 0;
 };
 
-class HumanPlayerStrategy: public PlayerStrategy{
+class HumanPlayerStrategy : public PlayerStrategy {
 public:
     HumanPlayerStrategy(Player *player);
+
     vector<Territory *> toAttack();
+
     vector<Territory *> toDefend();
+
     void issueOrder();
+
     string getType();
 };
 
-class NeutralPlayerStrategy: public PlayerStrategy{
+class AggressivePlayerStrategy : public PlayerStrategy {
+public:
+    AggressivePlayerStrategy(Player *player);
+
+    vector<Territory *> toAttack();
+
+    vector<Territory *> toDefend();
+
+    void issueOrder();
+
+    string getType();
+};
+
+class BenevolentPlayerStrategy : public PlayerStrategy {
+public:
+    BenevolentPlayerStrategy(Player *player);
+
+    vector<Territory *> toAttack();
+
+    vector<Territory *> toDefend();
+
+    void issueOrder();
+
+    string getType();
+};
+
+class NeutralPlayerStrategy : public PlayerStrategy {
 public:
     NeutralPlayerStrategy(Player *player);
+
     vector<Territory *> toAttack();
+
     vector<Territory *> toDefend();
+
     void issueOrder();
+
     string getType();
 
 };
+
+class CheaterPlayerStrategy : public PlayerStrategy {
+public:
+    CheaterPlayerStrategy(Player *player);
+
+    vector<Territory *> toAttack();
+
+    vector<Territory *> toDefend();
+
+    void issueOrder();
+
+    string getType();
+};
+
 #endif
